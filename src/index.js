ca5609ab8e57b45cfbb4bf5da5383495b025f507
Shahin Endregard Hemat <120106335+ShahinHemat@users.noreply.github.com>
2023-03-16 01:52:19 +0100

    Set up the backend
    With express JS and MongoDB.
    Also, tested that my code works so far, using thunderclient to make a post request to /auth/register

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// Setting up our server
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+// Connecting to our environment variable
+import dotenv from 'dotenv'; 
+dotenv.config();
+
+import { userRouter } from './routes/users.js';
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.use('/auth', userRouter);
+
+// Connecting to our database
+mongoose.connect(`mongodb+srv://shahin:${process.env.MY_DATABASE_PASSWORD}@recipes.swm0uee.mongodb.net/recipes?retryWrites=true&w=majority`);
+
+app.listen(3001, () => console.log('Server started on port 3001!'));
