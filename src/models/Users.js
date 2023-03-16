47c8a8255b1263fd60305114ee7e1b5a8e5bec4c
Shahin Endregard Hemat <120106335+ShahinHemat@users.noreply.github.com>
2023-03-16 14:44:24 +0100

    Routes for registering a new user and logging in

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+// This is like setting constraints/rules for the fields in your database column (here called 'users') in postgreSQL's CREATESCRIPT
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
